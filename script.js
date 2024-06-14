f610bd2bf358f78f18bf5951808d6f8d19452c17
Larvey03 <111690479+Larvey03@users.noreply.github.com>
2024-06-14 08:40:01 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+function smoothScroll(target) {
+    const element = document.getElementById(target);
+    if (element) {
+      window.scrollTo({
+        top: element.offsetTop,
+        behavior: 'smooth'
+      });
+    }
+  }
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    const anchors = document.querySelectorAll('a[href^="#"]');
+    anchors.forEach(function(anchor) {
+      anchor.addEventListener('click', function(event) {
+        event.preventDefault();
+        const target = anchor.getAttribute('href').substring(1);
+        smoothScroll(target);
+      });
+    });
+  });
+  
